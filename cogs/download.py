d5285e7755da945e864e88b7f5ec6dc2e16fcf7c
Bartuzen <ozenbartu@hotmail.com>
2019-07-15 06:30:55 +0300

    Use f-string in download cog

--- cogs/download.py
+++ cogs/download.py
@@ -12,6 +12,7 @@ class Download(discord.ext.commands.Cog):
         else:
             lang = self.bot.lang[self.bot.config["mainlang"]]
         mainlang = self.bot.lang[self.bot.config["mainlang"]]
+        downs = self.bot.downs
         addons = []
         if len(args) == 0:
             down = ["skript", "addon", "aliases", "paper", "spigot"]
@@ -38,43 +39,43 @@ class Download(discord.ext.commands.Cog):
                     for loop in self.bot.addonj:
                         if each.casefold() in loop.casefold():
                             ok = 1
-                            if "{}||{}".format(loop, self.bot.addonj[loop]["url"]) not in addons:
-                                addons.append("{}||{}".format(loop, self.bot.addonj[loop]["url"]))
+                            if f"{loop}||{self.bot.addonj[loop]['url']}" not in addons:
+                                addons.append(f"{loop}||{self.bot.addonj[loop]['url']}")
                                 break
                     if ok == 0:
-                        msg = await ctx.channel.send(embed=(discord.Embed(title="❌ {}".format(lang["errors"]["title"]), description=lang["downloads"]["unknown-file"].replace("%file%", each), color=self.bot.get_cog("Main").get_color("error"))))
+                        msg = await ctx.channel.send(embed=(discord.Embed(title=f"❌ {lang['errors']['title']}", description=lang["downloads"]["unknown-file"].replace("%file%", each), color=self.bot.get_cog("Main").get_color("error"))))
                         self.bot.messages.update({msg.id: {"command": self, "user": ctx.author.id, "message": ctx.id}})
                         try:
                             await msg.add_reaction("❌")
                         except (discord.errors.Forbidden, discord.errors.NotFound):
                             pass
                         return
-        embed = discord.Embed(title="{} {}".format(lang["downloads"]["emote"], lang["downloads"]["title"]), color=self.bot.get_cog("Main").get_color("downloads"))
+        embed = discord.Embed(title=f"{lang['downloads']['emote']} {lang['downloads']['title']}", color=self.bot.get_cog("Main").get_color("downloads"))
         if len(down) + len(addons) <= 20:
             if len(down) + len(addons) >= 1:
                 if "skript" in down:
                     skript = []
-                    for each in self.bot.downs["downloads"]["Skript"]:
+                    for each in downs["downloads"]["Skript"]:
                         if each != "latest":
                             if len(each.split("_")) > 1:
-                                skript.append("[Skript {}]({}) {}".format(each.split("_")[0], self.bot.downs["downloads"]["Skript"][each], each.split("_")[1]))
+                                skript.append(f"[Skript {each.split('_')[0]}]({downs['downloads']['Skript'][each]}) {each.split('_')[1]}")
                             else:
-                                skript.append("[Skript {}]({})".format(each, self.bot.downs["downloads"]["Skript"][each]))
+                                skript.append(f"[Skript {each}]({downs['downloads']['Skript'][each]})")
                         else:
-                            skript.append("[Skript {}]({})".format(lang["downloads"]["latest"], self.bot.downs["downloads"]["Skript"][each]))
+                            skript.append(f"[Skript {lang['downloads']['latest']}]({downs['downloads']['Skript'][each]})")
                     embed.add_field(name="Skript", value=" - ".join(skript), inline=False)
                 if "addon" in down:
                     addon = []
                     for each in self.bot.addonj:
                         if each.casefold() != "skript":
-                            addon.append("[{}]({})".format(each, self.bot.addonj[each]["url"]))
+                            addon.append(f"[{each}]({self.bot.addonj[each]['url']})")
                     addon.sort()
                     perfield = int(len(addon) / 6) + (0 if (len(addon) / 6).is_integer() else 1)
                     add = []
                     count = 0
                     for each in addon:
                         count += 1
-                        add.append("**{}.** {}".format(count, each))
+                        add.append(f"**{count}.** {each}")
                         if len(add) == perfield:
                             embed.add_field(name=(lang["downloads"]["addons"] if "name" not in locals() else "‎"), value="\n".join(add))
                             add.clear()
@@ -89,24 +90,24 @@ class Download(discord.ext.commands.Cog):
                         embed.add_field(name=lang["downloads"]["file-link"].replace("%file%", each.split("||")[0]), value=each.split("||")[1], inline=False)
                 if "aliases" in down:
                     aliases = []
-                    for each in self.bot.downs["downloads"]["aliases"]:
-                        aliases.append("[{}]({})".format(each, self.bot.downs["downloads"]["aliases"][each]))
+                    for each in downs["downloads"]["aliases"]:
+                        aliases.append(f"[{each}]({downs['downloads']['aliases'][each]})")
                     embed.add_field(name="Aliases", value=lang["downloads"]["aliases-desc"] + "\n" + " - ".join(aliases), inline=False)
                 if "paper" in down:
                     paper = []
-                    for each in self.bot.downs["downloads"]["Paper"]:
-                        paper.append("[{}](https://papermc.io/api/v1/paper/{}/latest/download)".format(each, each))
+                    for each in downs["downloads"]["Paper"]:
+                        paper.append(f"[{each}](https://papermc.io/api/v1/paper/{each}/latest/download)")
                     embed.add_field(name=lang["downloads"]["paper"], value=" - ".join(paper), inline=False)
                 if "spigot" in down:
-                    embed.add_field(name=lang["downloads"]["spigot"], value=self.bot.downs["downloads"]["Spigot"], inline=False)
+                    embed.add_field(name=lang["downloads"]["spigot"], value=downs["downloads"]["Spigot"], inline=False)
                 try:
                     msg = await ctx.channel.send(embed=embed)
                 except discord.errors.HTTPException:
-                    msg = await ctx.channel.send(embed=discord.Embed(title="❌ {}".format(lang["errors"]["title"]), description=lang["downloads"]["too-long"], color=self.bot.get_cog("Main").get_color("error")))
+                    msg = await ctx.channel.send(embed=discord.Embed(title=f"❌ {lang['errors']['title']}", description=lang["downloads"]["too-long"], color=self.bot.get_cog("Main").get_color("error")))
             else:
-                msg = await ctx.channel.send(embed=discord.Embed(title="❌ {}".format(lang["errors"]["title"]), description=lang["downloads"]["no-arg"], color=self.bot.get_cog("Main").get_color("error")))
+                msg = await ctx.channel.send(embed=discord.Embed(title=f"❌ {lang['errors']['title']}", description=lang["downloads"]["no-arg"], color=self.bot.get_cog("Main").get_color("error")))
         else:
-            msg = await ctx.channel.send(embed=discord.Embed(title="❌ {}".format(lang["errors"]["title"]), description=lang["downloads"]["too-long"], color=self.bot.get_cog("Main").get_color("error")))
+            msg = await ctx.channel.send(embed=discord.Embed(title=f"❌ {lang['errors']['title']}", description=lang["downloads"]["too-long"], color=self.bot.get_cog("Main").get_color("error")))
         self.bot.messages.update({msg.id: {"command": self, "user": ctx.author.id, "message": ctx.id}})
         try:
             await msg.add_reaction("❌")
